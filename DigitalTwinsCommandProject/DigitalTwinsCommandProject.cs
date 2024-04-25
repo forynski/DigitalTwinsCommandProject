f77b925b8ef649720f0ca3b00c532c2aa42b60a4
Robert Forynski <forynski@gmail.com>
2024-04-25 19:14:06 +0100

    sent only if true

--- DigitalTwinsCommandProject/DigitalTwinsCommandProject.cs
+++ DigitalTwinsCommandProject/DigitalTwinsCommandProject.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using Microsoft.Azure.Devices;
-using System.Text;
 using Newtonsoft.Json;
 
 namespace DigitalTwinsCommandProject
@@ -28,14 +28,22 @@ namespace DigitalTwinsCommandProject
                 // Fetch isThresholdExceeded value from the App Service
                 bool isThresholdExceeded = await FetchThresholdValueFromAppService();
 
-                // Create a message for IoT Hub
-                var message = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new { isThresholdExceeded })));
+                // Send the message to IoT Hub only if the threshold is exceeded
+                if (isThresholdExceeded)
+                {
+                    // Create a message for IoT Hub
+                    var message = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new { isThresholdExceeded })));
 
-                // Send the message to IoT Hub
-                var serviceClient = ServiceClient.CreateFromConnectionString(IOT_HUB_CONNECTION_STRING);
-                await serviceClient.SendAsync("iPhone", message); // Replace "your-device-id" with the ID of your IoT device
+                    // Send the message to IoT Hub
+                    var serviceClient = ServiceClient.CreateFromConnectionString(IOT_HUB_CONNECTION_STRING);
+                    await serviceClient.SendAsync("iPhone", message); // Replace "your-device-id" with the ID of your IoT device
 
-                log.LogInformation($"Message sent to IoT Hub with isThresholdExceeded value: {isThresholdExceeded}");
+                    log.LogInformation($"Message sent to IoT Hub with isThresholdExceeded value: {isThresholdExceeded}");
+                }
+                else
+                {
+                    log.LogInformation("Threshold not exceeded. No message sent to IoT Hub.");
+                }
             }
             catch (Exception ex)
             {
